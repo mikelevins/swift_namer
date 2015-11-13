0f0f6fa4f24bba754b6894734cefaa44252a3828
mikel evins <mevins@me.com>
2015-11-12 21:11:20 -0600

    Initial Commit

---
+++ namer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  namer
+//
+//  Created by mikel evins on 11/12/15.
+//  Copyright © 2015 mikel evins. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
